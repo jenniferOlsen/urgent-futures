3be9055d57e072117575b5b6d5d8c22c866234ce
Jenny Olsen <jenny.a.olsen@gmail.com>
2024-03-12 11:01:07 -0700

    welcome page setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<header>
+  <h1>Urgent Futures</h1>
+</header>
+<main class="container">
+  <slot />
+</main>
+
+<style>
+  header {
+    background-color: var(--pico-primary);
+    padding: var(--pico-typography-spacing-vertical);
+    margin-bottom: var(--pico-typography-spacing-vertical);
+  }
+  h1 {
+    color: var(--pico-primary-inverse);
+    text-align: center;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Long instructions will go here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<form>
+  <div class="form-group">
+    <label for="challenge">Choose your challenge</label>
+    <textarea name="challenge" id="challenge" placeholder="climate change, econmic inequality, racial injustice, brittle supply chains..." />
+  </div>
+  <div class="form-group">
+    <label for="community">Choose a community</label>
+    <input type="text" name="community" id="community" placeholder="my town, my neighborhood, my country, my workplace..."/>
+  </div>
+  <button type="submit">Find your future truth</button>
+</form>
