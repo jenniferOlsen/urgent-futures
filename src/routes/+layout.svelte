38238416ce31a79816b7004f92466b047bc4ac55
Jenny Olsen <jenny.a.olsen@gmail.com>
2024-03-14 09:33:42 -0700

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,9 @@
 <main class="container">
   <slot />
 </main>
+<footer class="container">
+  <small>Based on the Urgent Futures Questionaire from <a href="https://www.amazon.com/Imaginable-Create-Hopeful-Future_in-Community/dp/1954118333/"><em>Imaginable</em></a> by Jane McGonigal.</small>
+</footer>
 
 <style>
   header {
@@ -22,5 +25,9 @@
   h1 {
     color: var(--pico-primary-inverse);
     text-align: center;
+  }
+  footer {
+    text-align: center;
+    margin-bottom: var(--pico-typography-spacing-vertical);
   }
 </style>
