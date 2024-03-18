ca58689c66c064bca88f8aa276077e742d661e07
Jenny Olsen <jenny.a.olsen@gmail.com>
2024-03-18 11:23:22 -0700

    create range component

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,3 +2,7 @@ import { writable } from "svelte/store";
 
 export const challenge = writable('');
 export const community = writable('');
+export const calling = writable(0);
+export const belonging = writable(0);
+export const hope = writable(0);
+export const power = writable(0);
