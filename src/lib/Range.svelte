45872e4efe8682d41ad9c9b62a7b54eabd2d82cc
Jenny Olsen <jenny.a.olsen@gmail.com>
2024-03-19 10:33:35 -0700

    determine range binding by url path

--- src/lib/Range.svelte
+++ src/lib/Range.svelte
@@ -1,12 +1,32 @@
 <script>
-  import { calling } from "$lib/stores";
+  import { belonging, calling, hope, power } from "$lib/stores";
+  import { page } from '$app/stores';
+
+  let value = 1;
+
+  switch ($page.url.pathname) {
+    case '/calling':
+      value = $calling;
+      break;
+    case '/belonging':
+      value = $belonging;
+      break;
+    case '/hope':
+      value = $hope;
+      break;
+    case '/power':
+      value = $power;  
+      break;
+  }
+
 </script>
 <div class="range">
   <span>1</span>
-  <input aria-labelledby="question" aria-describedby="range-helper" type="range" min="1" max="10" bind:value={$calling} />
+  <input aria-labelledby="question" aria-describedby="range-helper" type="range" min="1" max="10" bind:value={value} />
   <span>10</span>
 </div>
 <small id="range-helper">1 = not at all, 10 = very much</small>
+
 
 <style>
   .range {
