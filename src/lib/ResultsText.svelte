0eee6fb13b9a9d18a4f58135f98f63016dc8f487
Jenny Olsen <jenny.a.olsen@gmail.com>
2024-03-20 11:26:25 -0700

    create results component

---
+++ src/lib/ResultsText.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { scale } from "svelte/transition";
+  import NavButtons from "$lib/NavButtons.svelte";
+</script>
+
+<div transition:scale>
+  <slot />
+  <NavButtons />
+</div>
--- src/routes/calling/+page.svelte
+++ src/routes/calling/+page.svelte
@@ -1,9 +1,8 @@
 <script>
   import ChallengeCheck from "$lib/ChallengeCheck.svelte";
-  import NavButtons from "$lib/NavButtons.svelte";
   import Range from "$lib/Range.svelte";
+  import ResultsText from "$lib/ResultsText.svelte";
   import { challenge, community, calling, showResults, reset } from "$lib/stores";
-  import { scale } from "svelte/transition";
 </script>
 
 <svelte:head>
@@ -12,16 +11,21 @@
 
 <ChallengeCheck>    
   <h2 id="question">How urgent do you think it is to address {$challenge} in {$community}?</h2>
+  
   <Range />
+
   {#if $showResults === true}
-    <div transition:scale>
+    <ResultsText>
       {#if $calling > 7} 
-        <p>You've found a specific long-term challenge that you're passionate about!</p>   
-        <p>The next question will help you determine how much help you expect to get from your community.</p>
+        <slot>
+          <p>You've found a specific long-term challenge that you're passionate about!</p>   
+          <p>The next question will help you determine how much help you expect to get from your community.</p>
+        </slot>
       {:else}
-        <p>It doesn't seem like you're really passionate about {$challenge}. That's fine! Consider <a href='/' on:click={() =>reset()}>choosing a different challenge.</a></p>
+        <slot>
+          <p>It doesn't seem like you're really passionate about {$challenge}. That's fine! Consider <a href='/' on:click={() =>reset()}>choosing a different challenge.</a></p>
+        </slot>
       {/if}
-      <NavButtons />
-    </div>
+    </ResultsText>
   {/if}
 </ChallengeCheck>
